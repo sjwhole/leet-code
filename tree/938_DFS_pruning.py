0854df50064016820132fc5c1f5f4dc96395d816
sjwhole <bluemoon2718@naver.com>
2021-07-24 21:07:48 +0900

    Range sum of BST - DFS with pruning

---
+++ tree/938_DFS_pruning.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        def dfs(node: TreeNode):
+            if node is None:
+                return 0
+
+            if node.val < low:
+                return dfs(node.right)
+            elif node.val > high:
+                return dfs(node.left)
+
+            return node.val + dfs(node.left) + dfs(node.right)
+
+        return dfs(root)
