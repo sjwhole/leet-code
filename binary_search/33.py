bb3e2433d48ee085f80bc4131bb4e89b62255594
sjwhole <bluemoon2718@naver.com>
2021-08-16 22:23:36 +0900

    Search in rotated sorted array

---
+++ binary_search/33.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        pivot = left
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_pivot = (mid + pivot) % len(nums)
+            if nums[mid_pivot] == target:
+                return mid_pivot
+            elif nums[mid_pivot] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
