733443f6c45f4ba8130bb2ac1209d267fbac603e
sjwhole <bluemoon2718@naver.com>
2021-10-13 11:41:56 +1000

    Best Time to Buy and Sell Stock II - Pythonic

---
+++ greedy/122_pythonic.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum(max(0, prices[i] - prices[i - 1]) for i in range(1, len(prices)))
